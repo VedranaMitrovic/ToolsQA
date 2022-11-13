c240648abc81bd90b5709a9af63ce9bdda0de04c
Korisnik <Korisnik@VEDRANA>
2022-11-13 22:58:44 +0100

    Add project files.

---
+++ ToolsQA/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolsQA
+{
+    internal class Program
+    {
+        IWebDriver driver = 
+        static void Main(string[] args)
+        {
+        }
+    }
+}
