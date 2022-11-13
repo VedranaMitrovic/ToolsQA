1c3dfa0d3f2bc1c50e9d0e6cd18158c6fb86087a
Korisnik <Korisnik@VEDRANA>
2022-11-13 23:48:32 +0100

    init

--- ToolsQA/Program.cs
+++ ToolsQA/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +8,35 @@ using System.Threading.Tasks;
 
 namespace ToolsQA
 {
-    internal class Program
+    public class Program
     {
-        IWebDriver driver = 
+        
         static void Main(string[] args)
         {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://demoqa.com/automation-practice-form");
+            driver.Manage().Window.Maximize();
+
+            IWebElement FirstName = driver.FindElement(By.Id("firstName"));
+            FirstName.SendKeys("Vedrana");
+
+            IWebElement LastName = driver.FindElement(By.Id("lastName"));
+            LastName.SendKeys("Mitrovic");
+
+            IWebElement Email = driver.FindElement(By.Id("userEmail"));
+            Email.SendKeys("vedrana.mitrovic@outlook.com");
+
+            IWebElement Mobile = driver.FindElement(By.Id("userNumber"));
+            Mobile.SendKeys("0638641853");
+
+            IWebElement CurrentAddress = driver.FindElement(By.Id("currentAddress"));
+            CurrentAddress.SendKeys("Savski nasip 7, Belgrade");
+
+            driver.Quit();
+
+
+
         }
+    
     }
 }
